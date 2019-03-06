520c63e62c5f61a8ee44dcb3c783441f6967aee7
hp <hp@LAPTOP-LLQV7Q0M>
2019-03-06 20:13:48 +0800

    实例化的HelloWorld

---
+++ Hello1/Hello1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello1
+{
+    class Line
+    {
+        private String GuestName { get; set; }
+        public Line(String str)
+        {
+            GuestName = str;
+        }
+        public void speak()
+        {
+            Console.WriteLine("Hello {0}!", GuestName);
+        }
+    }
+}
