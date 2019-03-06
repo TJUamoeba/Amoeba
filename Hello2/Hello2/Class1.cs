6a29ec5c90d293526c65ab2b2bfbe578c5da1fca
hp <hp@LAPTOP-LLQV7Q0M>
2019-03-06 20:55:35 +0800

    First Wersion

---
+++ Hello2/Hello2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2
+{
+    class Host
+    {
+        public static void speak(string str)
+        {
+            Console.WriteLine("Hello {0}!", str);
+        }
+    }
+}
---
+++ Hello2/Hello2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Hello! What's your name ?");
+            String GuestName = Console.ReadLine();
+            Host.speak(GuestName);
+            Console.ReadLine();
+
+        }
+    }
+}
